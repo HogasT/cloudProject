c8d5647ab263608f6963cf84aa88b8b0597d83f3
HogasT <hogastoma20@stud.ase.ro>
2024-05-09 12:43:23 +0300

    Added edit

--- cloud-project/components/MainPage.jsx
+++ cloud-project/components/MainPage.jsx
@@ -34,6 +34,9 @@ const MainPage = () => {
   const handleUpdateRecord = (id) => {
     router.push(`/records/edit?id=${id}`);
   };
+  const handleCreateRecord = () => {
+    router.push(`/records/create`);
+  }
 
   useEffect(() => {
     fetchRecords();
@@ -70,6 +73,13 @@ const MainPage = () => {
           </div>
         </div>
       ))}
+      <button
+              type="button"
+              className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              onClick={() => handleCreateRecord()}
+            >
+              Create
+            </button>
     </div>
   );
 };
--- cloud-project/components/RecordForm.jsx
+++ cloud-project/components/RecordForm.jsx
@@ -24,15 +24,15 @@ const RecordForm = (props) => {
             htmlFor="title"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Title
+            Maker
           </label>
           <input
             type="text"
             id="title"
             value={entry.title}
-            onChange={(e) => updateEntry("title", e.target.value)}
+            onChange={(e) => updateEntry("maker", e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Title"
+            placeholder="Maker"
             required
           />
         </div>
@@ -41,15 +41,15 @@ const RecordForm = (props) => {
             htmlFor="description"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Description
+            Model
           </label>
           <textarea
             id="description"
             rows="4"
             value={entry.description}
-            onChange={(e) => updateEntry("description", e.target.value)}
+            onChange={(e) => updateEntry("model", e.target.value)}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Write your thoughts here..."
+            placeholder="Model"
           ></textarea>
         </div>
         <div className="w-full flex justify-center gap-4">
